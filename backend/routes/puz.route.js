25dd65bebde5a7213537c2e0fa51ba9c6acd854e
RajTheGreatest <rajesh.respall@tup.edu.ph>
2025-03-01 10:21:06 +0800

    puzzle api

--- backend/routes/puz.route.js
+++ backend/routes/puz.route.js
@@ -1,15 +1,15 @@
-const express = require('express');
+const express = require("express");
 const {
     saveGameData,
     getUserGameData,
     getUserStats
-} = require('../controllers/puz.controller');
-const verifyToken = require('../middleware/auth.middleware.js');
+} = require("../controllers/puz.controller");
+const verifyToken = require("../middleware/auth.middleware.js");
 
 const router = express.Router();
 
-router.post('/', verifyToken, saveGameData);
-router.get('/user-games', verifyToken, getUserGameData);
-router.get('/user-stats', verifyToken, getUserStats);
+router.post("/", verifyToken, saveGameData);
+router.get("/user-games", verifyToken, getUserGameData);
+router.get("/user-stats", verifyToken, getUserStats);
 
 module.exports = router;

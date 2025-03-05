0578307e87460055e5a479c8d5d0d029b668676d
dayaannaa <carreondianaaa@gmail.com>
2025-03-05 23:39:37 +0800

    yung kagabi to sa institutions

--- backend/models/institution.model.js
+++ backend/models/institution.model.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 const institutionSchema = new mongoose.Schema({
-  id: {
-    type: Number,
-    required: true,
-    unique: true
-  },
   name: {
     type: String,
     required: true
@@ -15,8 +10,14 @@ const institutionSchema = new mongoose.Schema({
     required: true
   },
   mapEmbed: {
-    type: String
-  }
+    type: String,
+    required: true
+  },
+  institutionImage: {
+    type: String,
+    required: true,
+    default: ''
+  },
 });
 
 const Institution = mongoose.model('Institution', institutionSchema);
--- backend/routes/institution.route.js
+++ backend/routes/institution.route.js
@@ -2,12 +2,13 @@ const express = require('express');
 const { createInstitution, getAllInstitutions, getInstitutionById, updateInstitution, deleteInstitution } = require('../controllers/institution.controller');
 const verifyToken = require('../middleware/auth.middleware.js');
 
+
 const router = express.Router();
 
-router.post('/', verifyToken, createInstitution);
+router.post('/', createInstitution);
 router.get('/', verifyToken, getAllInstitutions);
 router.get('/:id', verifyToken, getInstitutionById);
-router.put('/:id', verifyToken, updateInstitution);
+router.put('/:id', verifyToken,  updateInstitution);
 router.delete('/:id', verifyToken, deleteInstitution);
 
 module.exports = router;

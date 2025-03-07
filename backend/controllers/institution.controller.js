867d7c70c3e83b8b15ee16fdf15cbfc58f8df273
dayaannaa <carreondianaaa@gmail.com>
2025-03-07 14:49:18 +0800

    institution all functional

--- backend/controllers/institution.controller.js
+++ backend/controllers/institution.controller.js
@@ -1,16 +1,22 @@
 const Institution = require('../models/institution.model');
+const { cloudinary, upload } = require('../config/cloudinary');
 
 class InstitutionController {
   async createInstitution(req, res) {
     const institution = req.body;
     try {
+      let imageUrl = null;
+      if (req.file) {
+        imageUrl = req.file.path;
+      }
       const newInstitution = new Institution({
         name: institution.name,
         address: institution.address,
-        mapEmbed: institution.mapEmbed  
+        mapEmbed: institution.mapEmbed,
+        institutionImage: imageUrl
       });
       await newInstitution.save();
-      res.status(201).json({status:true, newInstitution});
+      res.status(201).json({ status: true, newInstitution });
     } catch (error) {
       res.status(400).json({ message: 'Institution not created' });
     }
@@ -39,7 +45,11 @@ class InstitutionController {
 
   async updateInstitution(req, res) {
     try {
-      const institution = await Institution.findByIdAndUpdate(req.params.id, req.body, { new: true });
+      const updateData = req.body;
+      if (req.file) {
+        updateData.institutionImage = req.file.path;
+      }
+      const institution = await Institution.findByIdAndUpdate(req.params.id, updateData, { new: true });
       if (!institution) {
         return res.status(404).json({ message: 'Institution not found' });
       }
@@ -55,11 +65,13 @@ class InstitutionController {
       if (!institution) {
         return res.status(404).json({ message: 'Institution not found' });
       }
-      res.status(204).send();
+      res.status(200).json({ message: 'Institution deleted successfully' }); // Send response
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   }
+  
 }
 
 module.exports = new InstitutionController();
+module.exports.upload = upload;
